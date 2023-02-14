038c0e80a022f32b69ce31daf747a6b837fbd405
latif <latifyousuf513@gmail.com>
2023-02-14 19:31:23 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,322 @@
+//CHAPTER # 1
+// alert("HELLO WORLD");
+// alert("Error! Please enter a valid password.");
+// alert("Welcome to JS Land...\nHappy Coding!");
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!\nPrevent this page from creating additional dialogs.");
+// console.log("Hello...I can run JS through my web browser's console");
+// CHAPTER # 2
+// var username;
+// var MyName = "ABDUL LATIF";
+// var message = "Hello World";
+// alert(message);
+// var name = "ABDUL LATIF";
+// var age = "38 years old";
+// var qual = "Certified Moblie Application Development";
+// alert(name);
+// alert(age);
+// alert(qual);
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+// var email = ("latifyousuf513@gmail.com")
+// alert(email);
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from"+ " " + book);
+// document.write("Yah! I can write HTML content through JavaScript");
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+// CHAPTER # 3
+// var age = "I am 38 years old";
+// alert(age);
+// alert("You have visited this site 14 times");
+// var birthYear = ("my birth year is 1985");
+// document.write(birthYear);
+// document.write("<br>");
+// // document.write("Data type of my declared variable is number");
+// var Name = "JohnDoe ";
+// var Product = " T-Shirt(s)";
+// var quan = 5;
+// document.write(Name);
+// document.write("ordered ");
+// document.write(quan);
+// document.write(Product);
+// document.write(" on XYZ Clothing Store");
+
+//CHAPTER # 4
+// var one = "latif";
+// var two = "yousuf";
+// var three = 38;
+
+// legal veriable
+// var myname = latf;
+// var myName = latif;
+// var $myname = latf;
+// var _myname = latif;
+// var my_name = latif;
+
+// illegal variable
+
+// var 1myname = latif;
+// var my name = latif;
+// var !myname = latif;
+
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("<br>")
+// document.write("Variable names can only contain letters, $, _ and numbers\n");
+// document.write("<br>")
+// document.write("Variables must begin with a $, _ or letter");
+// document.write("<br>")
+// document.write("Variable names are case sensitive");
+// document.write("<br>")
+// document.write("Variable names should not be JS keywords");
+
+// CHAPTER # 5
+
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1+num2;
+// document.write("Sum of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + sum)
+
+// document.write("<br>")
+
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1-num2;
+// document.write("Sum of" + " " + num1 + " " + "-" + " " + num2 + " " + "is" + " " + sum)
+
+
+// document.write("<br>")
+
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1*num2;
+// document.write("Sum of" + " " + num1 + " " + "*" + " " + num2 + " " + "is" + " " + sum)
+
+// document.write("<br>")
+
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1/num2;
+// document.write("Sum of" + " " + num1 + " " + "/" + " " + num2 + " " + "is" + " " + sum)
+
+// document.write("<br>")
+
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1%num2;
+// document.write("Sum of" + " " + num1 + " " + "%" + " " + num2 + " " + "is" + " " + sum)
+
+// var name;
+// var name = latif;
+// var num = 38;
+// document.write(num);
+
+// var a = 5;
+// document.write(a);
+// document.write("<br>");
+// document.write(++a);
+// document.write("<br>");
+// var a7 = a + 7;
+// document.write(a7);
+// document.write("<br>");
+// document.write(--a7);
+// document.write("<br>");
+// document.write("The remainder is" + " " + a7%3);
+
+// var cost = 600;
+// var quan = 5;
+// var sum = cost*quan;
+// document.write("Total cost to buy" + " " + quan + "tickets to a movie is" + " " + sum);
+
+// var table = 4;
+// document.write(table + "x1 =" + " " + table);
+// document.write("<br>")
+// document.write(table + "x2 =" + " " + (table*2));
+// document.write("<br>")
+// document.write(table + "x3 =" + " " + (table*3));
+// document.write("<br>")
+// document.write(table + "x4 =" + " " + (table*4));
+// document.write("<br>")
+// document.write(table + "x5 =" + " " + (table*5));
+// document.write("<br>")
+// document.write(table + "x6 =" + " " + (table*6));
+// document.write("<br>")
+// document.write(table + "x7 =" + " " + (table*7));
+// document.write("<br>")
+// document.write(table + "x8 =" + " " + (table*8));
+// document.write("<br>")
+// document.write(table + "x9 =" + " " + (table*9));
+// document.write("<br>")
+// document.write(table + "x10 =" + " " + (table*10));
+
+// document.write("<h1>Shopping Cart</h1>")
+
+// var item1 = 650;
+// var item2 = 100;
+// var quan1 = 3;
+// var quan2 = 7;
+// var chg = 100;
+
+// document.write("Price of item 1 is" + " " + item1);
+// document.write("<br>")
+// document.write("Quantity of item 1 is" + " " + quan1);
+// document.write("<br>")
+// document.write("Price of item 2 is" + " " + item2);
+// document.write("<br>")
+// document.write("Quantity of item 2 is" + " " + quan2);
+// document.write("<br>")
+// document.write("Shipping Charges" + " " + chg);
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("Total cost of your order is" + " " + (item1*quan1 + item2*quan2 + chg));
+
+// var marks = 980;
+// var obtain = 804;
+// var percent = obtain*100/980;
+
+// document.write("<h1>Marks Sheet</h1>");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("Total marks:" + " " + marks);
+// document.write("<br>");
+// document.write("Total obtained:" + " " + obtain);
+// document.write("<br>");
+// document.write("Percentage:" + " " + percent);
+
+
+// var dollar = 104.80;
+// var riyal = 28;
+// var sum = 10*dollar+25*riyal;
+
+// document.write("<h1>Currency in PKR</h1>");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("Total currency in PKR: " + sum);
+
+// var current = 2023;
+// var birth = 1985;
+// var age = current-birth;
+// document.write("<h1>Age Calculator</h1>");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("Current Year: " + current);
+// document.write("<br>");
+// document.write("Birth Year: " + birth);
+// document.write("<br>");
+// document.write("Your age is: " + age);
+
+// var snack = "chocolate chip";
+// var age = 15;
+// var maxAge = 65;
+// var amount = 3;
+// var calc = (maxAge-age)*amount;
+
+// document.write("<h1>The Lifetime Supply Calculator</h1>");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("Favourite Snack: " + snack);
+// document.write("<br>");
+// document.write("current age: " + age);
+// document.write("<br>");
+// document.write("Estimated Maximum Age: " + maxAge);
+// document.write("<br>");
+// document.write("Amount of snacks per day: " + amount);
+// document.write("<br>");
+// document.write("You will need: " + calc + " " + snack + " to last you until the ripe old age of " + maxAge);
+
+// CHAPTER # 6-9
+
+// var a = 10;
+// document.write("The value of a is: " + a)
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// var inc1 = ++a
+// document.write("The value of ++a is: " + a)
+// document.write("<br>")
+// document.write("Now the value of a is: " + a)
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("The value of a++ is: " + a)
+// document.write("<br>")
+// var inc1 = a++
+// document.write("Now the value of a is: " + a)
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// var inc1 = --a
+// document.write("The value of --a is: " + a)
+// document.write("<br>")
+// document.write("Now the value of a is: " + a)
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("The value of a-- is: " + a)
+// var inc1 = a--
+// document.write("<br>")
+// document.write("Now the value of a is: " + a)
+
+// var a = 2;
+// var b = 1;
+// document.write(a);
+// document.write("<br>");
+// document.write(b);
+// document.write("<br>");
+// var result = --a - --b + ++b + b--;
+// //              1-0+1+1=3
+// document.write(result);
+
+// var name = prompt("enter your name");
+// alert(name);
+// alert("Welcome to site");
+
+// var num = +prompt("enter a number");
+// document.write(num + "x1 = " + num*1);
+// document.write("<br>");
+// document.write(num + "x2 = " + num*2);
+// document.write("<br>");
+// document.write(num + "x3 = " + num*3);
+// document.write("<br>");
+// document.write(num + "x4 = " + num*4);
+// document.write("<br>");
+// document.write(num + "x5 = " + num*5);
+// document.write("<br>");
+// document.write(num + "x6 = " + num*6);
+// document.write("<br>");
+// document.write(num + "x7 = " + num*7);
+// document.write("<br>");
+// document.write(num + "x8 = " + num*8);
+// document.write("<br>");
+// document.write(num + "x9 = " + num*9);
+// document.write("<br>");
+// document.write(num + "x10 = " + num*10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
